e6516fa69c1c70d84fd6c0a69e3fa98623c68aae
pakkarakarisaibalaji <56920717+pakkarakarisaibalaji@users.noreply.github.com>
2022-12-05 02:13:13 -0800

    Delete Digital_Book directory

--- Digital_Book/src/main/java/com/saibalaji/repository/IUserRepo.java
+++
@@ -1,9 +0,0 @@
-package com.saibalaji.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.saibalaji.entity.Users;
-
-public interface IUserRepo extends JpaRepository<Users, Integer> {
-
-}
